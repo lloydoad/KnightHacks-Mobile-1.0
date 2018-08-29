5e82b3fe71bf5160e2056d9b93012fd5415f0294
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2018-08-29 13:38:45 -0400

    Add color constant

--- KH_prototype_one/Constants.swift
+++ KH_prototype_one/Constants.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+// Colors
+let ORANGE_COLOR = UIColor(hex: 0xFF9500, alpha: 1)
+let BLUE_COLOR = UIColor(hex: 0x057AFF, alpha: 1)
+let GREEN_COLOR = UIColor(hex: 0x26AE60, alpha: 1)
+let RED_COLOR = UIColor(hex: 0xFF3B31, alpha: 1)
 let BACKGROUND_COLOR = UIColor(hex: 0x5755D6, alpha: 1)
+
 // assuming navbar has a default height of 44, 64 including status bar
 let NAVBAR_HEIGHT: CGFloat = 64
 // reduced total height of filter menu because menu took up a lot of space on smaller devices
