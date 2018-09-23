a85acd95e3bd63c82a4a73f853180f1455ddf419
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2018-09-23 13:59:23 -0400

    Remove scrollUpAnimation on ViewDidAppear to match all views
    Took out the initial hidden filter view because it was messing up other constraints. And the lack of animation in the liveCount and FrequentlyAsked views were mismatched

--- View/FilteredParentTableView.swift
+++ View/FilteredParentTableView.swift
@@ -59,14 +59,6 @@ class FilteredParentTableView: ParentTableView {
         filterButtons = childDelegate == nil ? [] : childDelegate!.setFilterMenuCellContents()
         tableViewCellContents = childDelegate == nil ? [:] : childDelegate!.setTableViewCellContents()
         tableViewHeaderTitles = childDelegate == nil ? [] : childDelegate!.setTableViewHeaderTitles()
-        
-        if !isBarAnimationComplete {
-            let yPointsBehindNavigationBar: CGFloat = 0
-            let offsetOfFilterMenu = CGPoint(x: 0, y: COMBINED_FILTER_HEIGHT - NAVBAR_HEIGHT - yPointsBehindNavigationBar)
-            self.tableView.setContentOffset(offsetOfFilterMenu, animated: false)
-            self.view.layoutIfNeeded()
-            self.view.layoutMarginsDidChange()
-        }
     }
     
     // Bring filter button menu cell back down
@@ -75,7 +67,6 @@ class FilteredParentTableView: ParentTableView {
         
         // if initial screen load, scroll table and filter menu
         if !isBarAnimationComplete {
-            self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .none, animated: true)
             let lastIndexInCollectionView = IndexPath(row: filterButtons.count - 1, section: 0)
             self.filterMenuCollectionViewReference.scrollToItem(at: lastIndexInCollectionView, at: .right, animated: true)
             isBarAnimationComplete = true
