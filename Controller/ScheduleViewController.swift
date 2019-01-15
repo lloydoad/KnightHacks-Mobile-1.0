4a1d25702b3ecdc03b8729f20ef4b6237b64c56e
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2019-01-15 05:06:21 -0500

    change setTableViewHeaders to use maps and cleanup

--- Controller/ScheduleViewController.swift
+++ Controller/ScheduleViewController.swift
@@ -10,19 +10,18 @@ import UIKit
 
 class ScheduleViewController: FilteredParentTableView, FilteredParentTableViewDelegate {
     let GET_SCHEDULE_URL: String = RequestSingleton.BASE_URL + "/api/get_schedule"
+    let DEFAULT_SCHEDULE_OBJECT: ScheduleObject = ScheduleObject(
+        title: "Title", eventType: Filter.activity.rawValue, location: "Location",
+        startTime: "2018-10-29T22:02:42.000Z", endTime: "2018-10-29T23:02:42.000Z"
+    )
     
     var orderedScheduleHeaders: [String:Int] = [:]
     var orderedScheduleObjects: [Int:[ScheduleObject]] = [:]
-    
     var allFetchedScheduleObjects: [ScheduleObject] = [] {
         didSet {
             filterScheduleObjects()
         }
     }
-    let defaultScheduleObject: ScheduleObject = ScheduleObject(
-        title: "Title", eventType: Filter.activity.rawValue, location: "Location",
-        startTime: "2018-10-29T22:02:42.000Z", endTime: "2018-10-29T23:02:42.000Z"
-    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,7 +69,46 @@ class ScheduleViewController: FilteredParentTableView, FilteredParentTableViewDe
         }
     }
     
-    func filterScheduleObjects(by filter: Filter = Filter.all) {
+    // override cells excluding filter menu cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section > 0 {
+            if let contentSection = orderedScheduleObjects[indexPath.section - 1] {
+                return makeCellFromModel(content: contentSection[indexPath.row], indexPath: indexPath)
+            } else {
+                return makeCellFromModel(content: DEFAULT_SCHEDULE_OBJECT, indexPath: indexPath)
+            }
+        } else {
+            return super.tableView(tableView, cellForRowAt: indexPath)
+        }
+    }
+    
+    func setFilterMenuCellContents() -> [FilterButton] {
+        return [
+            FilterButton(input: Filter.activity),
+            FilterButton(input: Filter.food),
+            FilterButton(input: Filter.talks),
+            FilterButton(input: Filter.workshops),
+            FilterButton(input: Filter.main_event),
+            FilterButton(input: Filter.all)
+        ]
+    }
+    
+    func setTableViewCellContents() -> [Int : [Any]] {
+        return orderedScheduleObjects
+    }
+    
+    func setTableViewHeaderTitles() -> [String] {
+        return (orderedScheduleHeaders.map {$0.value}).map {
+            orderedScheduleHeaders.key(forValue: $0) ?? "Header Error"
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        filterScheduleObjects(by: filterButtons[indexPath.row].type)
+        super.reloadTableContent()
+    }
+    
+    private func filterScheduleObjects(by filter: Filter = Filter.all) {
         var index: Int = 0
         orderedScheduleHeaders = [:]
         orderedScheduleObjects = [:]
@@ -100,57 +138,15 @@ class ScheduleViewController: FilteredParentTableView, FilteredParentTableViewDe
         super.reloadTableContent()
     }
     
-    func setFilterMenuCellContents() -> [FilterButton] {
-        return [
-            FilterButton(input: Filter.activity),
-            FilterButton(input: Filter.food),
-            FilterButton(input: Filter.talks),
-            FilterButton(input: Filter.workshops),
-            FilterButton(input: Filter.main_event),
-            FilterButton(input: Filter.all)
-        ]
-    }
-    
-    func setTableViewCellContents() -> [Int : [Any]] {
-        return orderedScheduleObjects
-    }
-    
-    func setTableViewHeaderTitles() -> [String] {
-        var headers: [String] = []
+    private func makeCellFromModel(content: ScheduleObject, indexPath: IndexPath) -> DynamicTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DynamicTableViewCell.identifier, for: indexPath) as! DynamicTableViewCell
         
-        for index in 0...orderedScheduleHeaders.count {
-            headers.append(orderedScheduleHeaders.key(forValue: index) ?? "Header Error")
-        }
+        cell.cellType = .defaultCell
+        cell.selectionStyle = .none
+        cell.titleLabel?.text = content.title
+        cell.locationLabel?.text = content.location
+        cell.timeLabel?.text = content.formattedTime
         
-        return headers
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        filterScheduleObjects(by: filterButtons[indexPath.row].type)
-        super.reloadTableContent()
-    }
-    
-    // override cells excluding filter menu cell
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section > 0 {
-            var content: ScheduleObject
-            let cell = tableView.dequeueReusableCell(withIdentifier: DynamicTableViewCell.identifier, for: indexPath) as! DynamicTableViewCell
-            
-            if let contentSection = orderedScheduleObjects[indexPath.section - 1] {
-                content = contentSection[indexPath.row]
-            } else {
-                content = defaultScheduleObject
-            }
-            
-            cell.cellType = .defaultCell
-            cell.selectionStyle = .none
-            cell.titleLabel?.text = content.title
-            cell.locationLabel?.text = content.location
-            cell.timeLabel?.text = content.formattedTime
-            
-            return cell
-        } else {
-            return super.tableView(tableView, cellForRowAt: indexPath)
-        }
+        return cell
     }
 }
