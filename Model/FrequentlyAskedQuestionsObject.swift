d489dcccd1e56214e3074bf93004238247b7a7d1
Jamal Yauhari Martinez <jamalyauharimcollege@gmail.com>
2019-01-15 22:37:47 -0500

    faq's update

---
+++ Model/FrequentlyAskedQuestionsObject.swift
@@ -0,0 +1,29 @@
+//
+//  FrequentlyAskedQuestionsObject.swift
+//  KnightHacks
+//
+//  Created by Jamal Yauhari on 1/15/19.
+//  Copyright © 2019 Lloyd Dapaah. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class FrequentlyAskedQuestionsObject {
+    
+    static let API_QUESTION_PARAMETER_KEY = "question"
+    static let API_ANSWER_PARAMETER_KEY = "answer"
+    
+    var question: String
+    var answer: String
+    
+    init(json: JSON) {
+        question = json[FrequentlyAskedQuestionsObject.API_QUESTION_PARAMETER_KEY].stringValue
+        answer = json[FrequentlyAskedQuestionsObject.API_ANSWER_PARAMETER_KEY].stringValue
+    }
+    
+    init(question: String, answer: String) {
+        self.question = question
+        self.answer = answer
+    }
+}
