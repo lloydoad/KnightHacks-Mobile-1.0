b7d5a9c337fa61340dd49db5361a4fbbaa9b8ffc
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2019-07-28 20:04:54 -0400

    add faq vc and cell

---
+++ Source/View Controller/FrequentlyAskedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FrequentlyAskedViewController.swift
+//  KnightHacks
+//
+//  Created by Lloyd Dapaah on 7/28/19.
+//  Copyright © 2019 KnightHacks. All rights reserved.
+//
+
+import UIKit
+
+internal class FrequentlyAskedViewController: NavigationBarTableViewController, ModelObserver {
+    
+    internal static let identifier: String = "FrequentlyAskedTableViewController"
+    
+    func didFetchModel() {
+        //
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
