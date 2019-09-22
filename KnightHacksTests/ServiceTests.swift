2598893d1a9a23c4f7e98208448cc2f2996cc526
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2019-09-21 22:48:39 -0400

    fix failing test

--- KnightHacksTests/ServiceTests.swift
+++ KnightHacksTests/ServiceTests.swift
@@ -62,44 +62,16 @@ class ServiceTests: XCTestCase {
         
         let dummyUrl = URL(string: "https://5d3a68a3fa091c0014470961.mockapi.io/connect/schedule")
         let singleton = RequestSingleton<CodableScheduleModel>()
-        let dateEngine = DateEngine(format: .standardISO1806)
-        
-        
         singleton.makeRequest(url: dummyUrl) { (results) in
             expectation.fulfill()
             if results == nil || results?.isEmpty ?? false {
                 XCTFail()
             } else {
-                var models: [ScheduleModel] = []
-                
-                results?.forEach {
-                    if let date = dateEngine.getDate(from: $0.date) {
-                        models.append(ScheduleModel(
-                            title: $0.title,
-                            location: $0.location,
-                            time: dateEngine.getString(of: date, as: .hourColonMinuteMeridian),
-                            header: dateEngine.getString(of: date, as: .dayMonth),
-                            date: date,
-                            filters: $0.filters.map { FilterNames(rawValue: $0)! }
-                        ))
-                    } else {
-                        XCTFail()
-                    }
-                }
-                
-                print(models)
                 expectation.fulfill()
             }
         }
         
         wait(for: [expectation], timeout: 10.0)
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
